d5e5af4a77f322495b001f581f5de1620fcce36f
rayhantmt <rayhantmt@gmail.com>
2025-09-03 01:31:47 +0600

    created a class and also an object of it

---
+++ class_object/baisc_class_object_syntax.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def displayinfo(self):
+            print(f"Name: {self.name}, Age: {self.age}")
+
+rayhan=Person("Rayhan", 19)
+rayhan.displayinfo()
