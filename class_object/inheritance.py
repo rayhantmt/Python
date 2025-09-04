0fed030d98bf47a04d573df0bb5f94264648d20c
rayhantmt <rayhantmt@gmail.com>
2025-09-05 03:35:36 +0600

    added the objects of the classes

--- class_object/inheritance.py
+++ class_object/inheritance.py
@@ -21,7 +21,36 @@ class mammal(Aminal):
             print(f"{self.name} is not warm blooded.")
     def speak(self):
         print(f"{self.name} of {self.specis} speaks.")
-birds = mammal("Bird","Reptile",is_warm_blooded=True)
-birds.idennity()
+#birds = mammal("Bird","Reptile",is_warm_blooded=True)
+#birds.idennity()
+class Bird(Aminal):
+    def __init__(self,name,specis,can_fly=True):
+        super().__init__(name,specis)
+        self.can_fly = can_fly
+    def fly(self):
+        if self.can_fly:
+            print(f"{self.name} is flying.")
+        else:
+            print(f"{self.name} is not flying.")
+    def speak(self):
+        print(f"{self.name} of {self.specis} speaks.")
+
+class Zookeeper():
+    def __init__(self,name):
+        self.name = name
+    def feed(self,animal):
+        print(f"{self.name} is feeding.{animal.name}.")
+    def put_to_sleep(self,animal):
+        print(f"{self.name} put to{animal.name} sleep.")
+    def move_animal(self,animal):
+        print(f"{self.name} moved {animal.name}.")
+snake=mammal("Snake","Reptail",is_warm_blooded=True)
+parrot=Bird("Parrot","Reptail",can_fly=True)
+fox=mammal("Fox","Reptail",is_warm_blooded=False)
+zookeeper=Zookeeper("Rahim")
+zookeeper.feed(fox)
+zookeeper.put_to_sleep(parrot)
+zookeeper.move_animal(snake)
+
 
 
