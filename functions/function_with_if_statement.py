3a18f0bc767b73a2bc5767333ce7cf2070b2b709
rayhantmt <rayhantmt@gmail.com>
2025-09-09 05:55:59 +0600

    successfuly created a function with if else statement

---
+++ functions/function_with_if_statement.py
@@ -0,0 +1,6 @@
+def chose(computer,human):
+    print(f'computer is {computer} and human is {human}')
+    if computer != human:
+        print('computer and human are equal')
+    return computer+human
+chose('electricity','pizza')
