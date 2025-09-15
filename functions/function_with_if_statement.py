c2011b2adefdb8bc2e64745c890b0094ce2af69e
rayhantmt <rayhantmt@gmail.com>
2025-09-15 23:35:42 +0600

    added the whole function

--- functions/function_with_if_statement.py
+++ functions/function_with_if_statement.py
@@ -4,3 +4,12 @@ def chose(computer,human):
         print('computer and human are equal')
     return computer+human
 chose('electricity','pizza')
+def chose(computer,human):
+    if computer != human:
+        print('computer and human are equal')
+        return computer+human
+    return computer
+chose('electricity','pizza')
+def chose(computer,human):
+    if computer == human:
+        print('computer and human are not equal')
