06a55bd6417d6494dd7f52333dd6c5f31020c135
rayhantmt <rayhantmt@gmail.com>
2025-09-25 05:55:21 +0600

    sorted the list case wise

--- list/combining_two_list.py
+++ list/combining_two_list.py
@@ -1,14 +1,14 @@
-bird=["parrot","john","john"]
-bird.extend(["crow","cow","hipopotemas"])
+bird=["Parrot","John","lea"]
+bird.extend(["Crow","cow","hipopotemas","a"])
 print(bird)
 print('The extend function adds another list with the list')
-bird.insert(3,"inserted")
+bird.insert(3,"a")
 print('The insert function adds an element at a specific position of the list')
 print(bird)
 x=bird.pop()
 print('The pop function returns the last element and removes it from the list')
 print(x)
-print(x)
+
 print(bird)
-bird.sort()
+bird.sort(key=str.upper())
 print(bird)
