b0525e3e5b22d55834d04ed5205ba0fa1ea595e8
rayhantmt <rayhantmt@gmail.com>
2025-10-05 21:29:10 +0600

    added the method ot check superset or subset

--- sets/stes.py
+++ sets/stes.py
@@ -4,3 +4,5 @@ intersect=names & singlename
 print(intersect)
 union=names | singlename
 print(union)
+superset=names>singlename#method to check wheather the set is super set or not
+print(superset)
