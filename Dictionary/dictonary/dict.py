092dfdf672e0479fbed85e0754f669694d674f07
rayhantmt <rayhantmt@gmail.com>
2025-10-05 21:21:35 +0600

    added the method to delete an item form the fict

--- Dictionary/dictonary/dict.py
+++ Dictionary/dictonary/dict.py
@@ -11,6 +11,8 @@ Boidata.pop('age')
 
 print(Boidata)
 Boidata['sex']='straight'#method to add a new item in the dict
+del Boidata['name']#method to delete an element form the dict
+
 print(Boidata)
 #using the list method helps to get the keys as a list
 print(list(Boidata.keys()))
