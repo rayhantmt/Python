07a1f558e2a124137a71ddd15dd740072e480e1a
rayhantmt <rayhantmt@gmail.com>
2025-10-07 11:19:01 +0600

    created a basic class and used it

---
+++ classes/basic_class.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def bark(self):
+       print("Dog barks ghew ghew")
+mydog=Dog("Doggy",7)
+print(mydog.name)
+print(mydog.age)
+mydog.bark()
