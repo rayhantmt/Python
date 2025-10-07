8426fd9fc756f6e46579cfdc472084f380c859c8
rayhantmt <rayhantmt@gmail.com>
2025-10-07 11:09:01 +0600

    added the emurate library in loop and used it

--- loops/for.py
+++ loops/for.py
@@ -1,6 +1,6 @@
 items=["flutter","React native","Django","python","dart"]
-for item in items:
-    print(item)
+for index ,item in enumerate(items):
+    print(index,item)
 def new():
     for item in range(13):
         print(item)
