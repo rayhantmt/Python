5e57fb99100abd3a7c503c8fb4d6b17288733f2f
rayhantmt <rayhantmt@gmail.com>
2025-10-07 11:21:46 +0600

    added a inherited class

--- classes/basic_class.py
+++ classes/basic_class.py
@@ -1,4 +1,8 @@
-class Dog:
+class Animal:
+    def walk(self):
+        print('Walking')
+
+class Dog(Animal):
     def __init__(self,name,age):
         self.name=name
         self.age=age
@@ -8,3 +12,4 @@ mydog=Dog("Doggy",7)
 print(mydog.name)
 print(mydog.age)
 mydog.bark()
+mydog.walk()
